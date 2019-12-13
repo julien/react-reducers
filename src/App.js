7ec58ce57915840b539a727022c3b9481b902794
Julien Castelain <julien.castelain@liferay.com>
2019-12-13 21:30:19 +0100

    Format everything with prettier

--- src/App.js
+++ src/App.js
@@ -1,26 +1,141 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useContext, useEffect, useReducer } from "react";
+
+const INITIAL_STATE = {
+	sidebarOpen: false,
+	sidebarPanelId: null
+};
+
+const StoreContext = React.createContext(INITIAL_STATE);
+
+const DEFAULT_CONFIG = {
+	sidebarPanels: [
+		[
+			{
+				sidebarPanelId: "one"
+			},
+			{
+				sidebarPanelId: "two"
+			}
+		],
+		[
+			{
+				sidebarPanelId: "three"
+			},
+			{
+				sidebarPanelId: "four"
+			}
+		]
+	]
+};
+
+const ConfigContext = React.createContext(DEFAULT_CONFIG);
+
+const DispatchContext = React.createContext(() => {});
+
+function reducer(state, action) {
+	let nextState;
+
+	switch (action.type) {
+		case "TOGGLE":
+			nextState = { ...state };
+
+			if (state.sidebarPanelId !== action.sidebarPanelId)
+				nextState.sidebarPanelId = action.sidebarPanelId;
+
+			if (state.sidebarOpen !== action.sidebarOpen)
+				nextState.sidebarOpen = action.sidebarOpen;
+
+			break;
+
+		case "SET_AND_TOGGLE":
+			if (
+				state.sidebarPanelId !== action.sidebarPanelId &&
+				state.sidebarOpen !== action.sidebarOpen
+			) {
+				nextState = {
+					...state,
+					sidebarPanelId: action.sidebarPanelId,
+					sidebarOpen: action.sidebarOpen
+				};
+			}
+			break;
+		default:
+			break;
+	}
+
+	return nextState;
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const config = useContext(DEFAULT_CONFIG);
+	const [store, dispatch] = useReducer(reducer, INITIAL_STATE);
+	const { sidebarPanels } = useContext(ConfigContext);
+	const buttons = sidebarPanels.reduce((acc, val) => acc.concat(val, []));
+
+	useEffect(() => {
+		const panel = sidebarPanels[0] && sidebarPanels[0][0];
+
+		if (panel) {
+			dispatch({
+				type: "SET_AND_TOGGLE",
+				sidebarPanelId: panel.sidebarPanelId,
+				sidebarOpen: true
+			});
+		} else {
+		}
+	}, []);
+
+	return (
+		<ConfigContext.Provider value={config}>
+			<StoreContext.Provider value={store}>
+				<DispatchContext.Provider value={dispatch}>
+					<div>
+						<ul>
+							{buttons.map((element, index) => (
+								<li
+									onClick={() => {
+										let sidebarOpen = store.sidebarOpen;
+										if (element.sidebarPanelId === store.sidebarPanelId) {
+											sidebarOpen = !store.sidebarOpen;
+										}
+
+										dispatch({
+											type: "TOGGLE",
+											sidebarOpen,
+											sidebarPanelId: element.sidebarPanelId
+										});
+									}}
+									key={`element-${index}`}
+								>
+									{element.sidebarPanelId}
+								</li>
+							))}
+						</ul>
+						<DummyButton />
+						<pre>{JSON.stringify(store, null, 0)}</pre>
+					</div>
+				</DispatchContext.Provider>
+			</StoreContext.Provider>
+		</ConfigContext.Provider>
+	);
+}
+
+function DummyButton() {
+	const dispatch = useContext(DispatchContext);
+
+	return (
+		<button
+			onClick={() => {
+				dispatch({
+					type: "TOGGLE",
+					sidebarOpen: true,
+					sidebarPanelId: "two"
+				});
+			}}
+		>
+			Dispatch an action from me
+		</button>
+	);
 }
 
 export default App;
